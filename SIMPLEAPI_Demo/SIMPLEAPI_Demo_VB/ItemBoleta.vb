eefa49b399412e97b40ce11dfee17354e4b9eb8a
michael vega diaz <mich.vega@alumnos.duoc.cl>
2021-03-10 09:03:43 -0300

    creación clase itemBoleta.vb

--- SIMPLEAPI_Demo/SIMPLEAPI_Demo_VB/ItemBoleta.vb
+++ SIMPLEAPI_Demo/SIMPLEAPI_Demo_VB/ItemBoleta.vb
@@ -1,3 +1,25 @@
 ﻿Public Class ItemBoleta
+    Public Property Nombre As String
+    Public Property Cantidad As Decimal
+    Public Property Precio As Integer
 
+    Public ReadOnly Property Total As Integer
+        Get
+            Return CInt(Math.Round(Cantidad * Precio, 0))
+        End Get
+    End Property
+
+    Public Property Afecto As Boolean
+    Public Property UnidadMedida As String
+    Public Property Descuento As Double
+    Public _tipoImpuesto As Integer
+
+    Public Property TipoImpuesto As ChileSystems.DTE.Engine.[Enum].TipoImpuesto.TipoImpuestoEnum
+        Get
+            Return If(_tipoImpuesto <> 0, CType(_tipoImpuesto, ChileSystems.DTE.Engine.[Enum].TipoImpuesto.TipoImpuestoEnum), ChileSystems.DTE.Engine.[Enum].TipoImpuesto.TipoImpuestoEnum.NotSet)
+        End Get
+        Set(ByVal value As ChileSystems.DTE.Engine.[Enum].TipoImpuesto.TipoImpuestoEnum)
+            _tipoImpuesto = CInt(value)
+        End Set
+    End Property
 End Class
