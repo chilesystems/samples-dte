20114a64b85f8ab7428db07eac44c0f5dc0351cd
michael vega diaz <mich.vega@alumnos.duoc.cl>
2021-03-15 09:11:59 -0300

    Implementación de eventos en ConsultaEstadoEnvioDTE.vb

--- SIMPLEAPI_Demo/SIMPLEAPI_Demo_VB/ConsultaEstadoEnvioDTE.vb
+++ SIMPLEAPI_Demo/SIMPLEAPI_Demo_VB/ConsultaEstadoEnvioDTE.vb
@@ -1,3 +1,31 @@
-﻿Public Class ConsultaEstadoEnvioDTE
+﻿Imports Newtonsoft.Json
+Imports SIMPLE_API.Enum.Ambiente
 
+Public Class ConsultaEstadoEnvioDTE
+
+    Dim handler As Handler = New Handler()
+
+    Private Sub ConsultaEstadoEnvioDTE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        handler.configuracion.LeerArchivo()
+        textRUTEmpresa.Text = handler.configuracion.Empresa.RutCuerpo.ToString()
+        textDVEmpresa.Text = handler.configuracion.Empresa.DV
+    End Sub
+
+    Private Sub botonConsultar_Click(sender As Object, e As EventArgs) Handles botonConsultar.Click
+        Dim trackId As Long = Long.Parse(textTrackID.Text)
+
+        Try
+
+            If radioEnvioDTE.Checked Then
+                Dim responseEstadoDTE = handler.ConsultarEstadoEnvio(If(radioProduccion.Checked, AmbienteEnum.Produccion, AmbienteEnum.Certificacion), trackId)
+                textRespuesta.Text = responseEstadoDTE.ResponseXml
+            Else
+                Dim responseEstadoDTE = handler.ConsultarEstadoEnvioBoleta(If(radioProduccion.Checked, AmbienteEnum.Produccion, AmbienteEnum.Certificacion), trackId)
+                textRespuesta.Text = JsonConvert.SerializeObject(responseEstadoDTE, Formatting.Indented)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show($"Ha ocurrido un error: {ex}")
+        End Try
+    End Sub
 End Class
