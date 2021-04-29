eb203923b8d926aadcb897606f262e2b3e8b4865
Michael Gonzalo vega diaz <mvega@chilesystems.com>
2021-04-29 12:24:55 -0400

    se cambio a carpeta EnvioDte

---
+++ SIMPLEAPI_Demo/DemoEndPoints/EnvioDTE/ConsultaDte.cs
@@ -0,0 +1,20 @@
+﻿using DemoEndPoints.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEndPoints.GenerarDTE
+{
+    public class ConsultaDte
+    {
+        public CertificadoDigital CertificadoDigital { get; set; }
+        public int RutEmpresa { get; set; }
+        public string RutEmpresaDigito { get; set; }
+        public int TrackId { get; set; }
+        public Boolean Produccion { get; set; }
+        public Boolean ServidorBoletaREST { get; set; }
+
+    }
+}
